6d6cf4f3e61d00d491c2e78bf63b82487f3907e5
snowmiku <1352218398a@gmail.com>
2025-10-21 10:14:46 +0000

    add new lvgl fixed dep

--- dep/etc/profile.d/S99game.sh
+++ dep/etc/profile.d/S99game.sh
@@ -4,4 +4,3 @@ export LANG=en_US.UTF-8
 stty iutf8 -F /dev/console
 export PATH=$PATH:/oem/pkpy/
 
-/bin/sh -c "/root/term_start_all.sh" > /dev/console
--- dep/root/game_logic.sh
+++ dep/root/game_logic.sh
@@ -40,3 +40,43 @@ fi
 /etc/init.d/S99lvgl start
 
 exit 0
+[root@luckfox ]# cat root/game_logic.sh
+#!/bin/sh
+
+# ==========================================================
+# == Main Boot Tasks (Will only run on the 2nd execution)
+# ==========================================================
+# Close the GUI launcher (Executed in father script)
+# /etc/init.d/S99lvgl stop
+# /etc/init.d/S99fbterm start_with_input
+
+# Prepare to launch the game
+echo "Starting Main Boot Tasks..."
+sleep 0.3
+
+# 1. Auto update
+# The output is redirected to /dev/console to be visible on the LCD screen.
+/usr/local/bin/copy_file.sh
+# Show the update log
+sleep 2
+
+# 2. Start Color test
+/usr/bin/color_test_c
+# Show color test information
+sleep 2
+clear
+
+# 3. Game
+echo "Start game file..."
+
+# Stuck process, e.g. the main game file
+cd /oem/brogue-rpg
+pocketpy ./main.py
+# evtest /dev/input/event1
+# stdio_test
+
+# Exit the son-script, and wait to restart the GUI
+echo "Game program exited..."
+sleep 2
+cd $HOME
+exit 0
